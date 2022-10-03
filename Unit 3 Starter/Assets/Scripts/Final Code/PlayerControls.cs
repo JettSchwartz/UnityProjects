cf84f6ccf769e3b221d074924da3d99cb50de52d
JettSchwartz <114099010+JettSchwartz@users.noreply.github.com>
2022-10-03 10:27:38 -0700

    roadRunner

--- Unit 3 Starter/Assets/Scripts/Final Code/PlayerControls.cs
+++ Unit 3 Starter/Assets/Scripts/Final Code/PlayerControls.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
+    private Rigidbody playerRB;
+
+    private void Awake()
+    {
+        playerRB = GetComponent<Rigidbody>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRB.AddForce(Vector3.up * 1000);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown.space)
     }
 }
