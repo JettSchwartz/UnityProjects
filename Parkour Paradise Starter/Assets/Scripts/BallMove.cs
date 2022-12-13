453ab2b7316907c0167ad1a159048c13abde7e81
JettSchwartz <jett.358754@nv.ccsd.net>
2022-12-12 20:23:22 -0800

    ugh

--- Parkour Paradise Starter/Assets/Scripts/BallMove.cs
+++ Parkour Paradise Starter/Assets/Scripts/BallMove.cs
@@ -4,15 +4,81 @@ using UnityEngine;
 
 public class BallMove : MonoBehaviour
 {
+    [SerializeField]
+    private Transform look;
+
+    [SerializeField]
+    private GameObject puIndicator;
+
+    private Rigidbody playerRB;
+    private float speed = 3.0f;
+    private float gravity = 1.0f;
+
+    private float jumpForce = 5f;
+    private bool isGrounded = true;
+
+    private void Awake()
+    {
+        playerRB = GetComponent<Rigidbody>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Physics.gravity *= gravity;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Make the powerup follow the player around
+        puIndicator.transform.position = transform.position + new Vector3(0f, 0.5f, 0f);
+        Move();
+        Jump();
+    }
+
+    private void Move()
+    {
+        // Get user input to control left, right, forward and back movement
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        // Move the player based on key presses
+        Vector3 dir = look.right * x + look.forward * z;
+
+        dir *= speed;
+        playerRB.AddForce(dir, ForceMode.Force);
+    }
+
+    private void Jump()
+    {
+        // If the space key is pressed and we are on the ground we jump
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
+        {
+            playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // When we collide back with the ground we can jump again
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+        // If the player collides the enemy and has the powerup, we will knockback the enemy
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+    }
+
+    // Controls how long the player has the powerup
+    private IEnumerator CountdownTimer()
+    {
+        yield return new WaitForSeconds(7);
+        puIndicator.SetActive(false);
     }
 }
---
+++ Parkour Paradise Starter/Assets/Scripts/LookAround.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAround : MonoBehaviour
+{
+    private float sensitivity = 10;
+
+    [SerializeField]
+    private Transform player;
+
+    private float newX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Hides our mouse
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Called at the end of each frame - prevent jittery camera movement
+    void LateUpdate()
+    {
+        // Get the mouse X and Y positions
+        float y = Input.GetAxis("Mouse X");
+        float x = Input.GetAxis("Mouse Y");
+
+        // Rotate the player left and right
+        player.eulerAngles += Vector3.up * y * sensitivity;
+        transform.eulerAngles += Vector3.up * y * sensitivity;
+
+        // Rotate the camera up and down
+        newX += x * sensitivity * -1f;
+        newX = Mathf.Clamp(newX, -25f, 50f);
+        transform.eulerAngles = new Vector3(newX, transform.eulerAngles.y, transform.eulerAngles.z);
+
+        // Make focal point follow player around
+        transform.position = player.position;
+    }
+}
