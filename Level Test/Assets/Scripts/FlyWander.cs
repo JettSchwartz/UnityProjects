b1682c70900e59c7bc6e4e97ddca753d6f3aa94d
JettSchwartz <114099010+JettSchwartz@users.noreply.github.com>
2023-01-26 10:47:36 -0800

    10:47 3rd

--- Level Test/Assets/Scripts/FlyWander.cs
+++ Level Test/Assets/Scripts/FlyWander.cs
@@ -16,7 +16,7 @@ public class FlyWander : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(ChangeDir());
     }
 
     // Update is called once per frame
@@ -27,7 +27,34 @@ public class FlyWander : MonoBehaviour
 
     private void Move()
     {
+        // apply force to move 
         myRB.AddForce(dir * flyForce, ForceMode.Force);
 
+        // Point forward
+        transform.forward = myRB.velocity;
+
+    }
+
+    private IEnumerator ChangeDir()
+    {
+        float newX;
+        float newZ;
+
+        float waitTime;
+
+        while (true)
+        {
+            // get a new direction
+            newX = Random.Range(-1.0f, 1.0f);
+            newZ = Random.Range(-1.0f, 1.0f);
+            dir = new Vector3(newX, 0, newZ);
+
+            // get new speed
+            flyForce = Random.Range(1.0f, 3.0f);
+
+            // wait random number of time before changing direction again
+            waitTime = Random.Range(1.0f, 4.0f);
+            yield return new WaitForSeconds(waitTime);
+        }
     }
 }
