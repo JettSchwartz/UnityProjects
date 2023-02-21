e29b5df7637fe0b7c47f4203078c6914980d45c1
JettSchwartz <jett.358754@nv.ccsd.net>
2023-02-20 18:51:36 -0800

    almost done

---
+++ Level Test/Assets/Scripts/Floor Is Lava/Look.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Look : MonoBehaviour
+{
+    private float sensitivity = 10f;
+
+    [SerializeField]
+    private Transform player;
+
+    private float newX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        float y = Input.GetAxis("Mouse X");
+        float x = Input.GetAxis("Mouse Y");
+
+        player.eulerAngles += Vector3.up * y * sensitivity;
+        transform.eulerAngles += Vector3.up * y * sensitivity;
+
+        transform.position = player.position + new Vector3(0, 0, -2);
+    }
+}
---
+++ Level Test/Assets/Scripts/Floor Is Lava/MovePlayer.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    [SerializeField]
+    private Transform look;
+
+    private Rigidbody playerRB;
+    private float speed = 10.0f;
+
+    private void Awake()
+    {
+        playerRB = GetComponent<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        // Get user input to control left, right, forward and back movement
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        // Move the player based on key presses
+        Vector3 dir = look.right * x + look.forward * z;
+
+        dir *= speed;
+        playerRB.AddForce(dir, ForceMode.Force);
+    }
+}
